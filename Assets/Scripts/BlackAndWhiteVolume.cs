20ceed9b71c92cba47ac88a8e30a0daf9cf721c4
Awxccc <sancturez@gmail.com>
2026-02-10 03:11:38 +0800

    yes

--- Assets/Scripts/BlackAndWhiteVolume.cs
+++ Assets/Scripts/BlackAndWhiteVolume.cs
@@ -16,40 +16,51 @@ public class Settings
 [VolumeComponentMenu("Custom/Black And White")]
 public class BlackAndWhiteVolume : VolumeComponent, IPostProcessComponent
 {
-    public ClampedFloatParameter intensity = new ClampedFloatParameter(1f, 0f, 1f);
+    public ClampedFloatParameter intensity = new(0f, 0f, 1f);
+
     public bool IsActive() => intensity.value > 0f;
+
     public bool IsTileCompatible() => true;
 }
 public class BlackAndWhiteRendererFeature : ScriptableRendererFeature
 {
-    
+    [System.Serializable]
+    public class Settings
+    {
+        public RenderPassEvent passEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+        public Shader shader;
+    }
+
     [SerializeField] private Settings settings = new();
     private Material _material;
     private Pass _pass;
-}
 
-public override void Create()
-{
-    if (settings.shader == null)
+    public override void Create()
     {
-        settings.shader = Shader.Find("Hidden/Custom/BlackAndWhite");
-    }
-    if(settings.shader != null)
-    {
-            _material = CoreUtils.CreateEngineMaterial(settings.shader);
-    }
-    _pass = new Pass(_material)
-    {
-        renderPassEvent = settings.passEvent
-    };
-}
-    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
-{
-    if(_material == null)
-    {
-        return;
+        if (settings.shader == null)
+        {
+            settings.shader = Shader.Find("Hidden/Custom/BlackAndWhite");
         }
-    renderer.EnqueuePass(_pass);
+
+        if (settings.shader != null)
+        {
+            _material = CoreUtils.CreateEngineMaterial(settings.shader);
+        }
+
+        _pass = new Pass(_material)
+        {
+            renderPassEvent = settings.passEvent
+        };
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        if (_material == null)
+        {
+            return;
+        }
+
+        renderer.EnqueuePass(_pass);
     }
 
     protected override void Dispose(bool disposing)
@@ -57,22 +68,95 @@ public override void Create()
         _pass?.Dispose();
         CoreUtils.Destroy(_material);
     }
+
     private sealed class Pass : ScriptableRenderPass
-{
-    private readonly Material _mat;
-    public Pass(Material mat)
     {
-        _mat = mat;
+        private readonly Material _mat;
+
+        private class CopyPassData
+        {
+            public TextureHandle source;
+        }
+
+        private class BwPassData
+        {
+            public TextureHandle source;
+            public Material material;
+            public float intensity;
+        }
+
+        public Pass(Material mat)
+        {
+            _mat = mat;
+        }
+
+        public void Dispose() { }
+        public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+        {
+            if (_mat == null)
+            {
+                return;
+            }
+
+            var cameraData = frameData.Get<UniversalCameraData>();
+            if (cameraData.isPreviewCamera)
+            {
+                return;
+            }
+
+            var bw = VolumeManager.instance.stack.GetComponent<BlackAndWhiteVolume>();
+            if (bw == null || !bw.IsActive())
+            {
+                return;
+            }
+
+            float intensity = bw.intensity.value;
+
+            var resources = frameData.Get<UniversalResourceData>();
+            if (resources.isActiveTargetBackBuffer)
+            {
+                return;
+            }
+
+            TextureHandle target = resources.activeColorTexture;
+            if (!target.IsValid())
+            {
+                return;
+            }
+
+            var desc = renderGraph.GetTextureDesc(target);
+            desc.clearBuffer = false;
+            desc.name = "BW_TempCopy";
+
+            TextureHandle tempCopy = renderGraph.CreateTexture(desc);
+
+            using (var builder = renderGraph.AddRasterRenderPass<CopyPassData>("BW Copy", out var passData))
+            {
+                builder.UseTexture(target, AccessFlags.Read);
+                builder.SetRenderAttachment(tempCopy, 0);
+                passData.source = target;
+
+                builder.SetRenderFunc((CopyPassData data, RasterGraphContext ctx) =>
+                {
+                    Blitter.BlitTexture(ctx.cmd, data.source, new Vector4(1f, 1f, 0f, 0f), 0f, false);
+                });
+            }
+
+            using (var builder = renderGraph.AddRasterRenderPass<BwPassData>("BW Apply", out var passData))
+            {
+                builder.UseTexture(tempCopy, AccessFlags.Read);
+                builder.SetRenderAttachment(target, 0);
+                passData.source = tempCopy;
+                passData.material = _mat;
+                passData.intensity = intensity;
+
+                builder.SetRenderFunc((BwPassData data, RasterGraphContext ctx) =>
+                {
+                    data.material.SetFloat("_Intensity", data.intensity);
+                    Blitter.BlitTexture(ctx.cmd, data.source, new Vector4(1f, 1f, 0f, 0f), data.material, 0);
+                });
+            }
+        }
     }
 }
 
-public void Dispose()
-{
-}
-public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
-{
-    if(_mat == null)
-    {
-        return;
-    }
-}
